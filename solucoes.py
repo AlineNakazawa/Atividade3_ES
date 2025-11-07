daa19dac151be7186e13cf86e0417e00ee05cc9a
Ana Carla <anacarlarosa@alunos.utfpr.edu.br>
2025-11-07 17:13:10 -0300

    feat: implement logic for caesar cipher

--- solucoes.py
+++ solucoes.py
@@ -3,8 +3,31 @@ def anagrama(string1, string2):
     pass
 
 def cifra_cesar(texto, deslocamento):
-    # TODO
-    pass
+    """
+    Aplica a Cifra de César ao texto, preservando minúsculas/maiúsculas e caracteres não-letras.
+    """
+    resultado = ""
+    deslocamento = deslocamento % 26 
+
+    for char in texto:
+        if 'a' <= char <= 'z':
+            inicio_alfabeto = ord('a')
+            posicao_original = ord(char) - inicio_alfabeto
+            nova_posicao = (posicao_original + deslocamento) % 26
+            novo_char = chr(inicio_alfabeto + nova_posicao)
+            resultado += novo_char
+            
+        elif 'A' <= char <= 'Z':
+            inicio_alfabeto = ord('A')
+            posicao_original = ord(char) - inicio_alfabeto
+            nova_posicao = (posicao_original + deslocamento) % 26
+            novo_char = chr(inicio_alfabeto + nova_posicao)
+            resultado += novo_char
+            
+        else:
+            resultado += char
+            
+    return resultado
 
 def valida_cpf(cpf):
     # TODO
